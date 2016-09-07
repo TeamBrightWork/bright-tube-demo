fb161a906caa181ab72a3a7de261cf5f208679e9
Rob Bruhn <rdbruhn@gmail.com>
2016-09-07 13:26:39 -0500

    (enhancement) Add upload wireframe and video elements to playlist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,33 +24,57 @@ angular.module('bwTubeDemoApp')
 
       controller.currentVideo++;
 
-      if (controller.currentVideo >= controller.videos.length) controller.currentVideo = 0;
+      if (controller.currentVideo >= $scope.videos.length) controller.currentVideo = 0;
 
       controller.setVideo(controller.currentVideo);
     };
 
-    controller.videos = [
-      {
-        sources: [
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-        ]
-      },
-      {
-        sources: [
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov"), type: "video/mp4"},
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/big_buck_bunny_720p_stereo.ogg"), type: "video/ogg"}
-        ]
-      }
-    ];
+    controller.getVideos = function() {
+
+      // TODO: Get Video Data from DB
+
+      // $bw.models.video.find().then(function(videos) {
+      //   console.log(videos);
+      //   //filter through each video and add as source to controller.videos
+      //   $scope.$apply();
+      // });
+
+      // FAKE VIDEO DATA
+      var videosFromDB = [
+        {
+          url: "http://static.videogular.com/assets/videos/videogular.mp4",
+          type: "video/mp4",
+          name: "Pale Blue Dot"
+        },
+        {
+          url: "http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov",
+          type: "video/mp4",
+          name: "Big Buck Bunny"
+        }
+      ];
+
+      $scope.videos = [];
+
+      videosFromDB.forEach(function(video){
+
+        video = {
+          sources:[
+            {src: $sce.trustAsResourceUrl(video.url), type: video.type, name: video.name},
+          ]
+        };
+
+        $scope.videos.push(video);
+      })
+    };
+
+    controller.getVideos();
 
     controller.config = {
       preload: "none",
       autoHide: false,
       autoHideTime: 3000,
       autoPlay: false,
-      sources: controller.videos[0].sources,
+      sources: $scope.videos[0].sources,
       theme: {
         url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
       },
@@ -62,18 +86,8 @@ angular.module('bwTubeDemoApp')
     controller.setVideo = function(index) {
       controller.API.stop();
       controller.currentVideo = index;
-      controller.config.sources = controller.videos[index].sources;
+      controller.config.sources = $scope.videos[index].sources;
       $timeout(controller.API.play.bind(controller.API), 100);
     };
-
-    controller.getVideos = function() {
-      $bw.models.video.find().then(function(videos) {
-        console.log(videos);
-        //filter through each video and add as source to controller.videos
-        $scope.$apply();
-      });
-    };
-
-    controller.getVideos();
 
   }]);
--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -8,55 +8,126 @@
  * Controller of the bwTubeDemoApp
  */
 angular.module('bwTubeDemoApp')
-  .controller('UploadCtrl', ['$scope', 'FileUploader', function ($scope, FileUploader) {
+  .controller('UploadCtrl', ['$scope', 'FileUploader', '$bw', '$sce', function ($scope, FileUploader, $bw, $sce) {
+
+    var files = [];
+    $scope.tags = [];
+    var uploadedVideoID;
+
+    $scope.fileAdded = false;
+    $scope.isUploading = false;
+    $scope.uploaded = false;
+
+    var headers = {
+      // TODO: Add object Storage Headers
+    };
 
     var uploader = $scope.uploader = new FileUploader({
-      url: ''
+      url: '/',
+      // alias: 'file',
+      headers: headers,
+      // queue: [],
+      // progress: 0,
+      // autoUpload: false,
+      removeAfterUpload: true,
+      method: 'POST',
+      // filters: [],
+      // formData: [],
+      // queueLimit: Number.MAX_VALUE,
+      // withCredentials: false,
+      // disableMultipart: false
+      autoUpload: true
     });
 
-    // FILTERS
+    $scope.clickUpload = function(){
+      document.getElementById('upload').click();
+    };
 
-    uploader.filters.push({
-      name: 'customFilter',
-      fn: function(item /*{File|FileLikeObject}*/, options) {
-        return this.queue.length < 10;
-      }
-    });
+    var saveVideoData = function(videoData){
+
+      // TODO: Store Video data in DB
+      console.log(videoData);
+      // return $bw.models.video.create(video).then(function(newVideo){
+      //   console.log(newVideo);
+      //
+      //   tags.forEach(function(tag){
+      //     $bw.models.tag.create(tag).then(function(newTag){
+      //       console.log(newTag);
+      //     });
+      //   });
+      //
+      //   return newVideo.id;
+      // });
+
+      return null;
+    };
+
+    $scope.addTag = function(){
+
+      // TODO: Store tag data in DB using uploadedVideoID
+
+      $scope.tags.push($scope.tag);
+      $scope.tag = '';
+    };
+
+    $scope.removeTag = function(name){
+      console.log('this',this.tag)
+      var toRemove = this.tag;
+
+      // TODO: Remove tag data from DB
+
+      $scope.tags.forEach(function(tag, idx){
+        if(tag === toRemove){
+          $scope.tags.splice(idx, 1);
+        }
+      });
+    };
 
     // CALLBACKS
 
     uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
       console.info('onWhenAddingFileFailed', item, filter, options);
     };
+
     uploader.onAfterAddingFile = function(fileItem) {
       console.info('onAfterAddingFile', fileItem);
+      $scope.fileAdded = fileItem;
     };
-    uploader.onAfterAddingAll = function(addedFileItems) {
-      console.info('onAfterAddingAll', addedFileItems);
-    };
+
     uploader.onBeforeUploadItem = function(item) {
       console.info('onBeforeUploadItem', item);
+      $scope.isUploading = true;
     };
-    uploader.onProgressItem = function(fileItem, progress) {
-      console.info('onProgressItem', fileItem, progress);
-    };
-    uploader.onProgressAll = function(progress) {
-      console.info('onProgressAll', progress);
-    };
+
     uploader.onSuccessItem = function(fileItem, response, status, headers) {
       console.info('onSuccessItem', fileItem, response, status, headers);
+      response.url
+
+      var videoData = {
+        url: response.url,
+        name: response.name,
+        type: response.type
+      };
+
+      saveVideoData(videoData).then(function(newVideoId){
+        console.log("Video Stored with Id: ", newVideoId);
+        uploadedVideoID = newVideoId;
+      });
+
+      $scope.isUploading = false;
+      $scope.uploaded = true;
+
     };
+
     uploader.onErrorItem = function(fileItem, response, status, headers) {
       console.info('onErrorItem', fileItem, response, status, headers);
+      // TODO: remove on once wired up to object storage
+      $scope.isUploading = false;
+      $scope.uploaded = $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4');
     };
-    uploader.onCancelItem = function(fileItem, response, status, headers) {
-      console.info('onCancelItem', fileItem, response, status, headers);
-    };
+
     uploader.onCompleteItem = function(fileItem, response, status, headers) {
       console.info('onCompleteItem', fileItem, response, status, headers);
-    };
-    uploader.onCompleteAll = function() {
-      console.info('onCompleteAll');
     };
 
     console.info('uploader', uploader);
